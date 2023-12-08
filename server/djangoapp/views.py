9d4af4376de15ed92306eb199b680a1a7765f512
oussama-ghandour <oussam92.ing@gmail.com>
2023-12-08 12:31:40 -0500

    add_static_pages

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -15,10 +15,9 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
-
-
 # Create an `about` view to render a static about page
-# def about(request):
+def about_view(request):
+    return render(request, 'djangoapp/about.html')
 # ...
 
 

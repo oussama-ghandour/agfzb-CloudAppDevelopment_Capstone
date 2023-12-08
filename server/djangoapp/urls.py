a38925751497fb0d96b16c627ce073b23e204678
oussama-ghandour <oussam92.ing@gmail.com>
2023-12-08 12:59:22 -0500

    add_navigation_bar

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-     path('about/', views.about_view, name='about'),
+    path('about/', views.about_view, name='about'),
     # path for contact us view
-
+    path('contact/',views.contact_view, name='contact'),
     # path for registration
 
     # path for login
